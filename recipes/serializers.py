0ee37f94e53b930c79b99d43ea53b916a55d20e6
Thiago Costa <thiagoocdiniz@gmail.com>
2023-12-27 14:31:24 -0300

    Metodo de renomear em DRF

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -5,3 +5,8 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
+    public = serializers.BooleanField(source='is_public')
+    preparetion = serializers.SerializerMethodField(method_name='any_method')
+    
+    def any_method(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
