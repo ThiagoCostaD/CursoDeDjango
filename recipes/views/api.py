d999c50c21286a79dcef6c0fff276f82d4773d6a
Thiago Costa <thiagoocdiniz@gmail.com>
2023-12-27 11:17:34 -0300

    criando list e datail em DRF

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,7 +1,27 @@
+from django.shortcuts import get_object_or_404
+from models import Recipe
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from ..serializers import RecipeSerializer
 
 
 @api_view()
 def recipe_api_list(request) -> Response:
-    return Response('ok')
+    recipes = Recipe.objects.get_published()[:10]
+    serializer = RecipeSerializer(
+        instance=recipes,
+        many=True
+    )
+    return Response(serializer.data)
+
+@api_view()
+def recipe_api_detail(request, pk) -> Response:
+    recipes = get_object_or_404(
+        Recipe.objects.get_published(),
+        pk=pk
+    )
+    
+    serializer = RecipeSerializer(instance=recipes, many=False)
+    return Response(serializer.data)
