2d270173d8f72638c38ce2a47421d5f69fecd5cc
Thiago Costa <thiagoocdiniz@gmail.com>
2024-01-03 14:55:01 -0300

    -

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,6 +1,6 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
-
-from .models import Category
+from tags.models import Tag
 
 
 class RecipeSerializer(serializers.Serializer):
@@ -10,8 +10,13 @@ class RecipeSerializer(serializers.Serializer):
     public = serializers.BooleanField(source='is_public')
     preparetion = serializers.SerializerMethodField(method_name='any_method')
     
-    category =  serializers.PrimaryKeyRelatedField(
-        queryset = Category.objects.all()
+    category =  serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=User.objects.all()
     )
     
     def any_method(self, recipe):
